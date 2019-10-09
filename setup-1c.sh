a8618fdafb7f470b1fc19001756927fd78038f4e
renbuar <renbuar@gmail.com>
2019-10-09 17:55:56 +0300

    setup-1c.sh

---
+++ setup-1c.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+sudo rm -rf dist
+mkdir dist
+echo "Please enter https://releases.1c.ru/ credentials"
+#IFS=""
+read -p "Login: " USERNAME
+read -p "Password: " PASSWORD
+read -p "Version: " VERSION
+
+export FIRST=1 #1 первоначальная установка #0 дополнительные установки
+export USERNAME
+export PASSWORD
+export VERSION
+export PORT=1540
+export REGPORT=1541
+export RANGE=1560:1690
+export RASPORT=1545
+export USR1CV8=usr1cv8
+export GROUP1CV8=grp1cv8 
+#export SERVICENAME=usr1cv8-
+#export SERVICENAME=v
+export SERVICENAME=obraz
+
+
+./download.sh
+./build.sh
